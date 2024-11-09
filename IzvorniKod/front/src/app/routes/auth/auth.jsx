8654a091577320d0576eb0247e2455fe1458cd7b
davidvodopija <david.vodopija@fer.hr>
2024-11-09 19:37:23 +0100

    Adds header for auth page

--- IzvorniKod/front/src/app/routes/auth/auth.jsx
+++ IzvorniKod/front/src/app/routes/auth/auth.jsx
@@ -1,5 +1,18 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "../../../globalStyles.css";
+import { useParams } from "react-router-dom";
+
+const { mode } = useParams();
 function Auth() {
-	return <></>;
+	return (
+		<>
+			<div className="d-flex flex-column align-items-center">
+				<div className="h-auth">PORTANE</div>
+				<hr className="hr-auth" />
+			</div>
+		</>
+	);
 }
 
 export default Auth;
