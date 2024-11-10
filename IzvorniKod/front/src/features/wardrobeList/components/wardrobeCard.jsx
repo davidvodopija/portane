21a0bf30dea44bafd84a9d15e2bfb6a65a3617fb
topaloviccc <lara.topalovic@fer.hr>
2024-11-10 19:16:57 +0100

    Fixed css for wardrobeCard

--- IzvorniKod/front/src/features/wardrobeList/components/wardrobeCard.jsx
+++ IzvorniKod/front/src/features/wardrobeList/components/wardrobeCard.jsx
@@ -5,21 +5,23 @@ import CloseButton from "../../../components/closeButton/closeButton";
 
 function WardrobeCard() {
 	return (
-		<div className="card">
-			<div className="close-button-wrapper">
-				<CloseButton></CloseButton>
-			</div>
-			<img
-				src={wardrobeLogo}
-				className="card-img-top mx-auto"
-				alt="..."
-			/>
-			<div className="card-body mx-auto">
-				<p className="card-text">Ormar1 (n)</p>
-				<div className="text-secondary">
-					<div> Polica: x</div>
-					<div> Ladica: y</div>
-					<div> Šipki za odjeću: z</div>
+		<div className="wardrobe-card">
+			<div className="card">
+				<div className="close-button-wrapper">
+					<CloseButton></CloseButton>
+				</div>
+				<img
+					src={wardrobeLogo}
+					className="card-img-top mx-auto"
+					alt="Wardrobe icon"
+				/>
+				<div className="card-body mx-auto">
+					<p className="card-text">Ormar1 (n)</p>
+					<div className="text-secondary">
+						<div> Polica: x</div>
+						<div> Ladica: y</div>
+						<div> Šipki za odjeću: z</div>
+					</div>
 				</div>
 			</div>
 		</div>
