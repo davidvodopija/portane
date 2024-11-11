4779a9ef6dc797e55b5be14e6b92e82df9fb085c
Boban067 <borna.l@icloud.com>
2024-11-11 03:52:18 +0100

    Carousel with automated transition added

--- IzvorniKod/front/src/app/routes/app/home.jsx
+++ IzvorniKod/front/src/app/routes/app/home.jsx
@@ -1,7 +1,13 @@
 import Header from "../../../components/header/header";
+import CarouselAuto from "../../../components/carousel/carouselAuto";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function Home() {
-	return <Header></Header>;
+	return <>
+		<Header></Header>
+		<CarouselAuto/>
+	</>;
 }
 
 export default Home;
---
+++ IzvorniKod/front/src/components/carousel/carouselAuto.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./carouselAuto.css";
+import Button from "../button/button";
+
+export const CarouselAuto = () => {
+  return (
+    <div id="carouselExampleSlidesOnly" className="carousel slide d-flex" data-bs-ride="carousel" data-bs-interval="5000">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="https://picsum.photos/id/856/800/600" class="d-block w-100" alt="Slide 1"/>
+        </div>
+        <div className="carousel-item">
+          <img src="https://picsum.photos/id/823/800/600" class="d-block w-100" alt="Slide 2"/>
+        </div>
+        <div className="carousel-item">
+          <img src="https://picsum.photos/id/604/800/600" class="d-block w-100" alt="Slide 3"/>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row row-cols-1">
+          <a>Posloži,</a>
+          <a>Pretraži,</a>
+          <a>Prodaj,</a>
+        </div>
+        <div className="naslov">PORTANE</div>
+        <div className="btnContainer"><Button size="small" color="red" radius="rounded">Otvori vrata svog stila!</Button></div>
+      </div>
+    </div>
+  );
+}
+
+export default CarouselAuto;
