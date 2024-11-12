c69348c3cc708dc0b692b5e6d76432ba58f0bd0f
topaloviccc <lara.topalovic@fer.hr>
2024-11-12 01:50:28 +0100

    Fixed and protected routing

--- IzvorniKod/front/src/app/router.jsx
+++ IzvorniKod/front/src/app/router.jsx
@@ -1,21 +1,27 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./routes/app/home";
 import UserProfile from "./routes/app/userProfile";
 import CreateWardrobe from "./routes/app/createWardrobe";
 import Auth from "./routes/auth/auth";
 import AppProvider from "./provider";
+import PrivateRoutes from "./routes/privateRoutes";
 
 function Router() {
 	return (
 		<BrowserRouter>
 			<AppProvider>
 				<Routes>
+					<Route element={<PrivateRoutes />}>
+						<Route path="/user-profile" element={<UserProfile />} />
+						<Route
+							path="/create-wardrobe"
+							element={<CreateWardrobe />}
+						/>
+					</Route>
 					<Route path="/" element={<Home />} />
-					<Route path="/user-profile" element={<UserProfile />} />
-					{/*path should maybe later be change to actual username*/}
-					<Route path="/create-wardrobe" element={<CreateWardrobe />} />
 					<Route path="/auth/:mode" element={<Auth />} />
 					<Route path="/auth" element={<Auth />} />
+					<Route path="*" element={<Navigate to="/" replace />} />
 				</Routes>
 			</AppProvider>
 		</BrowserRouter>
