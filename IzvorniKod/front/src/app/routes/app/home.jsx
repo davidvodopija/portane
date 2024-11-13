28c5e083374762f037e5897bdc50a8538d92d375
Boban067 <borna.l@icloud.com>
2024-11-13 03:05:12 +0100

    Added footer

--- IzvorniKod/front/src/app/routes/app/home.jsx
+++ IzvorniKod/front/src/app/routes/app/home.jsx
@@ -2,6 +2,7 @@ import Header from "../../../components/header/header";
 import CarouselAuto from "../../../components/carousel/carouselAuto";
 import Categories from "../../../components/categories/categories";
 import Items from "../../../components/items/items";
+import Footer from "../../../components/footer/footer";
 
 function Home() {
 	return <>
@@ -9,6 +10,7 @@ function Home() {
 		<CarouselAuto/>
 		<Categories/>
 		<Items/>
+		<Footer/>
 	</>;
 }
 
