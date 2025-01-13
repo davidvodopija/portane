323727a6694173aff4ba5a02dca80ddfcb2d3e2a
davidvodopija <david.vodopija@fer.hr>
2025-01-13 04:55:48 +0100

    Add wardrobe part manipulation

--- IzvorniKod/front/src/app/routes/app/editWardrobe.jsx
+++ IzvorniKod/front/src/app/routes/app/editWardrobe.jsx
@@ -1,12 +1,33 @@
 import PrimaryHeading from "../../../components/primaryHeading/primaryHeading";
 import Header from "../../../components/header/header";
 import WardrobePartsList from "../../../features/wardrobePartsList/components/wardrobePartsList";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getAllWardrobes } from "../../../features/wardrobeList/api/wardrobesAPI";
 
 function EditWardrobe() {
+	const { wardrobeId } = useParams();
+	const [title, setTitle] = useState("");
+
+	useEffect(() => {
+		getAllWardrobes()
+			.then((response) => {
+				response.forEach((part) => {
+					if (part.id == wardrobeId) {
+						setTitle(part.title);
+					}
+				});
+			})
+			.catch((error) => {
+				console.error("Error getting wardrobe parts:", error);
+			});
+	}),
+		[wardrobeId];
+
 	return (
 		<>
 			<Header />
-			<PrimaryHeading text="ORMAR 1" />
+			<PrimaryHeading text={title} />
 			<WardrobePartsList />
 		</>
 	);
--- IzvorniKod/front/src/app/routes/app/wardrobeView.jsx
+++ IzvorniKod/front/src/app/routes/app/wardrobeView.jsx
@@ -3,12 +3,33 @@ import Header from "../../../components/header/header";
 import WardrobeItemsList from "../../../features/wardrobeView/components/wardrobeItemsList";
 import WardrobeControls from "../../../features/wardrobeView/components/wardrobeControls";
 import "./wardrobeView.css";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getAllWardrobes } from "../../../features/wardrobeList/api/wardrobesAPI";
 
 function WardrobeView() {
+	const { wardrobeId } = useParams();
+	const [title, setTitle] = useState("");
+
+	useEffect(() => {
+		getAllWardrobes()
+			.then((response) => {
+				response.forEach((part) => {
+					if (part.id == wardrobeId) {
+						setTitle(part.title);
+					}
+				});
+			})
+			.catch((error) => {
+				console.error("Error getting wardrobe parts:", error);
+			});
+	}),
+		[wardrobeId];
+
 	return (
 		<>
 			<Header />
-			<PrimaryHeading text="ORMAR 1" />
+			<PrimaryHeading text={title} />
 
 			<div className="wardrobe-view d-flex">
 				<WardrobeItemsList />
