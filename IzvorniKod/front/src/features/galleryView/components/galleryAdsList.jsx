affd542848859f01eab7855c0994210dfad5bd5a
topaloviccc <lara.topalovic@fer.hr>
2025-01-24 14:43:24 +0100

    Map widget fix

--- IzvorniKod/front/src/features/galleryView/components/galleryAdsList.jsx
+++ IzvorniKod/front/src/features/galleryView/components/galleryAdsList.jsx
@@ -1,54 +1,58 @@
-import {useEffect, useState, useContext} from "react";
+import { useEffect, useState, useContext } from "react";
 import "../../wardrobeView/components/wardrobeItemsList.css";
 import GalleryAd from "./galleryAd";
-import {useParams} from "react-router-dom";
-import {getAllGalleryItems} from "../api/galleryAdsAPI";
-import {galleriesContext} from "../../galleriesList/context/galleriesContext";
-import {postAdvertSearch} from "../../itemSearch/api/categorySelectAPI.jsx";
+import { useParams } from "react-router-dom";
+import { getAllGalleryItems } from "../api/galleryAdsAPI";
+import { galleriesContext } from "../../galleriesList/context/galleriesContext";
+import { postAdvertSearch } from "../../itemSearch/api/categorySelectAPI.jsx";
 import Paging from "../../../components/paging/paging.jsx";
 
 function GalleryAdsList() {
-    const [ads, setAds] = useState([]);
-    const {galleryId} = useParams();
-    const {getGalleries} = useContext(galleriesContext);
-    const [page, setPage] = useState(0);
-    const [totalPages, setTotalPages] = useState(0);
-    let size = 12;
+	const [ads, setAds] = useState([]);
+	const { galleryId } = useParams();
+	const { getGalleries } = useContext(galleriesContext);
+	const [page, setPage] = useState(0);
+	const [totalPages, setTotalPages] = useState(0);
+	let size = 12;
 
+	useEffect(() => {
+		postAdvertSearch({ galleryId: galleryId }, page, size)
+			.then((data) => {
+				setAds(data.result.content);
+				setTotalPages(data.result.totalPages);
+				setPage(data.result.pageable.pageNumber);
+			})
+			.catch((error) => console.error(error));
+	}, [page]);
 
-    useEffect(() => {
-        postAdvertSearch({galleryId: galleryId}, page, size).then((data) => {
-            setAds(data.result.content);
-            setTotalPages(data.result.totalPages);
-            setPage(data.result.pageable.pageNumber);
-        }).catch((error) => console.error(error))
-    }, [page]);
+	const handleAdDelete = (deletedAdId) => {
+		getAllGalleryItems(galleryId)
+			.then((data) => setAds(data))
+			.catch((error) => console.error(error));
+		getGalleries();
+	};
 
-    const handleAdDelete = (deletedAdId) => {
-        getAllGalleryItems(galleryId)
-            .then((data) => setAds(data))
-            .catch((error) => console.error(error));
-        getGalleries();
-    };
-
-    return (
-        <div className="d-flex w-100 pe-sm-0 pe-lg-5 flex-wrap justify-content-start">
-            <div className="ms-4">
-                <Paging totalPages={totalPages} setCurrentPage={setPage} currentPage={page}></Paging>
-            </div>
-            <div className="wardrobe-items-container d-flex flex-wrap mx-5 mt-3 mb-5">
-                {ads.map((ad) => (
-                    <GalleryAd
-                        key={ad.id}
-                        id={ad.id}
-                        galleryId={galleryId}
-                        onItemDeleted={handleAdDelete}
-                    />
-                ))}
-            </div>
-        </div>
-
-    );
+	return (
+		<div className="d-flex w-100 pe-sm-0 pe-lg-5 flex-wrap justify-content-start">
+			<div className="ms-4">
+				<Paging
+					totalPages={totalPages}
+					setCurrentPage={setPage}
+					currentPage={page}
+				></Paging>
+			</div>
+			<div className="wardrobe-items-container d-flex flex-wrap mx-5 mt-3 mb-5 w-100">
+				{ads.map((ad) => (
+					<GalleryAd
+						key={ad.id}
+						id={ad.id}
+						galleryId={galleryId}
+						onItemDeleted={handleAdDelete}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default GalleryAdsList;
